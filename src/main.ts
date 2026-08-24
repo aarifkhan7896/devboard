20f55c330811d8c35d545e4117e23dceebd26a25
Aarif Khan <aarifkhan7896@gmail.com>
2026-08-24 17:18:25 +0530

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Devboard')
+    .setDescription('The devboard API documentation')
+    .setVersion('1.0')
+    .addTag('Devboard')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
